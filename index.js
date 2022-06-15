1fbd0e4606080d0e78841fe4cd79038842c1242f
unknown <apriadi1163@gmail.com>
2022-06-16 02:51:01 +0700

    update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express = require("express");
 require("dotenv").config();
+const express = require("express");
 const cors = require("cors");
 const http = require("http");
 const { Server } = require("socket.io");
@@ -8,7 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000", // define client origin if both client and server have different origin
+    origin: process.env.CLIENT_URL, // define client origin if both client and server have different origin
   },
 });
 // require("./src/socket")(io);
@@ -22,5 +22,9 @@ app.use(express.json());
 app.use(cors());
 app.use("/api/v1/", router);
 app.use("/uploads", express.static("uploads"));
-
+app.get("/", function (req, res) {
+  res.send({
+    message: "Hello World",
+  });
+});
 server.listen(port, () => console.log(`listening on port ${port}`));
